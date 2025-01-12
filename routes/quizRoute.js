8556c45e0b6030cc5f5b57ad08bc97e1c8cd5a98
Raj Singh <rajsingh.19.vns@gmail.com>
2025-01-13 02:25:27 +0530

    Developed the verify user middleware

--- routes/quizRoute.js
+++ routes/quizRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const verifyUser = require("../middleware/verifyUser");
 const { createQuizHandler, allQuizzesHandler, getSubsByCatHandler, getSubByIdHandler } = require("../controller/quizController");
 
 router.post('/admin/quiz', createQuizHandler);
 router.get('/quizzes', allQuizzesHandler);
 router.get('/quiz', getSubsByCatHandler);
-router.get('/quiz/sub/:id', getSubByIdHandler);
+router.get('/quiz/sub/:id', verifyUser, getSubByIdHandler);
 
 module.exports = router;
